b8baac3e5c64980a1acdaa597a4a4cd8b75729cf
Israel-199 <israelassefa199@gmail.com>
2025-08-30 07:45:56 -0700

    initial commit

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(frontendDist));
 
   // SPA fallback
-  app.get("*", (_, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(frontendDist, "index.html"));
   });
 }
