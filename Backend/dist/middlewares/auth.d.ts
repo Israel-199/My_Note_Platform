f115f66c6dc6384060cbd6db42702cb0e2927628
Israel-199 <israelassefa199@gmail.com>
2025-09-30 01:29:03 -0700

    final version

--- Backend/dist/middlewares/auth.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { IUser } from '../models/User.js';
-interface AuthRequest extends Request {
-    user?: IUser;
-}
-export declare const authenticateToken: (req: AuthRequest, res: Response, next: NextFunction) => Promise<void>;
-export { AuthRequest };
-//# sourceMappingURL=auth.d.ts.map
