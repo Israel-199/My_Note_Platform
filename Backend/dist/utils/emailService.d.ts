7d645597a423088d190a629a83058fbecb779896
Israel-199 <israelassefa199@gmail.com>
2025-09-30 23:48:40 -0700

    final version

--- Backend/dist/utils/emailService.d.ts
+++
@@ -1,8 +0,0 @@
-declare class EmailService {
-    private transporter;
-    constructor();
-    sendOTP(email: string, otp: string, purpose: 'signup' | 'signin'): Promise<void>;
-}
-declare const _default: EmailService;
-export default _default;
-//# sourceMappingURL=emailService.d.ts.map
