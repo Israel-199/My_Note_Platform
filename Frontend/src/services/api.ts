e5cd5d2e01590fc7e019c0d87e277d3ce8085710
Israel-199 <israelassefa199@gmail.com>
2026-08-29 02:49:34 -0700

    feat: implement axios instance and auth/notes API services with 401 interceptor

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { SignupData, SigninData } from '../types';
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL;
+  'https://my-note-platform-8-pziu.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
